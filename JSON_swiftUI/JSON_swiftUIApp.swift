f4b956734c1b3801d6d51bc0878b83d58ec2d29e
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2023-06-19 11:00:24 -0500

    [add] - Initial commit

---
+++ JSON_swiftUI/JSON_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  JSON_swiftUIApp.swift
+//  JSON_swiftUI
+//
+//  Created by Pierre Juarez U. on 19/06/23.
+//
+
+import SwiftUI
+
+@main
+struct JSON_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
